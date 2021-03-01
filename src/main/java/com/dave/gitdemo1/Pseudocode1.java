97b3ee808a27fe84dfa8cf9dd9f3c19500049c8a
Davemon <dave8898@qq.com>
2021-02-28 23:21:12 -0500

    version1.0

--- src/main/java/com/dave/gitdemo1/Pseudocode1.java
+++ src/main/java/com/dave/gitdemo1/Pseudocode1.java
@@ -2,6 +2,6 @@ package com.dave.gitdemo1;
 
 public class Pseudocode1 {
     public static void main(String[] args){
-        System.out.println("PseudoCode");
+        System.out.println("PseudoCode 222333");
     }
 }
